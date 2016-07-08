7dd5a7fdff2a4b4ca74547732279493e243b8211
mahaamu <mahalakshmi3901@gmail.com>
2016-07-08 14:50:47 +0530

    Update and rename README.md to README.c
    finish

---
+++ README.c
@@ -0,0 +1,15 @@
+# d
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a;
+printf("enter the value");
+scanf("%d",&a);
+if(a%4==0)
+{
+printf("it is leap year");
+else
+printf("it is not leap year");
+}
+}
